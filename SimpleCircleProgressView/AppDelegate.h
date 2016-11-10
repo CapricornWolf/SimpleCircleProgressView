0626d721bbb7467e32af5a016adf5a3a385db297
jason <1069345726@qq.com>
2016-11-10 15:48:11 +0800

    Initial Commit

---
+++ SimpleCircleProgressView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleCircleProgressView
+//
+//  Created by Jason on 16/11/10.
+//  Copyright © 2016年 com.JaosnQJl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleCircleProgressView/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SimpleCircleProgressView
+//
+//  Created by Jason on 16/11/10.
+//  Copyright © 2016年 com.JaosnQJl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+                        
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
